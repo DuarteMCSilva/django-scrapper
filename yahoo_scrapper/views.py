be5a7a9fba13173097c118ea827218c9443b049a
DuarteMCSilva <duartemcsilva@gmail.com>
2024-08-02 17:56:17 +0200

    Get historical prices API

--- yahoo_scrapper/views.py
+++ yahoo_scrapper/views.py
@@ -25,22 +25,31 @@ def getData(request):
     request_ticker: str = request.GET.get('ticker')
     period: str = request.GET.get('p')
 
+    history_df = get_historical_prices(request_ticker, period)
+
+    if history_df.index.name == 'Date':
+        history_df = history_df.reset_index()
+    history_df['Date'] = pd.to_datetime(history_df['Date'])
+    history_df['Date'] = history_df['Date'].dt.strftime('%Y%m%d')
+
+    key_value_map = history_df.set_index('Date')['Close'].to_dict()
+
+    json_response = json.dumps(key_value_map, indent=4)
+
+    return JsonResponse(json_response, safe=False)
+
+def get_historical_prices(ticker: str = None, period: str = ''):
+
     possibleVals = ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
 
-    if request_ticker == None: 
+    if ticker == None: 
         return HttpResponse('No ticker provided!')
     
     if period not in possibleVals:
         period = "5y"
 
-    ticker = yfinance.Ticker(request_ticker)
-    history_df = ticker.history(period=period)
-
-
-    history_json = history_df.to_json()
-
-    history_dict = json.loads(history_json)
-    return JsonResponse(history_dict, safe=False)
+    ticker = yfinance.Ticker(ticker)
+    return ticker.history(period=period)
 
 def getGraph(request):
     request_ticker: str = request.GET.get('ticker')
